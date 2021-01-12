d58a7301dad1ccb7b3c9e49b544f23abea95fbf7
Kanskje <kanskje7@protonmail.ch>
2021-01-12 18:58:46 -0300

    First edit

---
+++ bot.py
@@ -0,0 +1,137 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.members = True
+intents = intents.all()
+client = commands.Bot(command_prefix="?", intents=intents)
+client.remove_command("help")
+
+filtered_words = ['puta', 'arrombado', 'buceta', 'pinto', 'caralho', 'idiota', 'escroto', 'porra']
+
+
+@client.event
+async def on_ready():
+    print("Bot online!")
+    print(client.user.name)
+    print(client.user.id)
+
+
+@client.event
+async def on_message(msg):
+    if msg.author == client.user:
+        return
+    for word in filtered_words:
+        if word in msg.content:
+            await msg.delete()
+            break ## stops it constantly spamming / continues the loop
+
+    if "noob" in msg.content.lower():
+        await msg.add_reaction("<:mds:703304861575544962>")
+        await msg.add_reaction("<:pikoh:606574166497558538>")
+    if "nub" in msg.content.lower():
+        await msg.add_reaction("<:mds:703304861575544962>")
+        await msg.add_reaction("<:pikoh:606574166497558538>")
+    if "Formulário" in msg.content.lower():
+         await msg.add_reaction("<:desconfiadx:610229151840075786>")
+    if "Formulario" in msg.content.lower():
+         await msg.add_reaction("<:desconfiadx:610229151840075786>")
+    if "austin" in msg.content.lower():
+        await msg.add_reaction("🇳")
+        await msg.add_reaction("🇴")
+        await msg.add_reaction("🇧")
+
+    await client.process_commands(msg)
+
+
+@client.event
+async def on_message_delete(message):
+    if message.author.bot:
+        return
+    channel = client.get_channel(id=797203131019689994) #id do canal em que as mensagens serão arquivadas
+    await channel.send(f'{message.author} deleted this message:\n{message.content}')
+
+@client.event
+async def on_member_join(member):
+    channel = client.get_channel(id=782802548212891658) #verifique aqui
+
+    await channel.send(f'Bem vindo ao servidor oficial dos Helpers BR {member.mention}! Por favor escreva seu nick no jogo '
+          f'utilizando esses caracteres para a tag ﹟ ₀ ₁ ₂ ₃ ₄ ₅ ₆ ₇ ₈ ₉ . Não se esqueça de ler as regras em <#515924836724506634>'
+          f'e divirta-se! Para mais comandos digite ?help no canal <#789349633121845249>')
+
+
+@client.command(aliases=['sobre'])
+async def Sobre(ctx):
+    await ctx.send('Olá! Eu sou um bot experimental e estou aqui para te ajudar! Fui programado pelo Kanskje e espero '
+                   'fazer um bom trabalho!')
+
+
+@client.command(aliases=['recrutamento'])
+async def Recrutamento(ctx):
+    await ctx.send('O recrutamento para a equipe dos Helpers BR está aberto! Para saber mais detalhes, entre nesse '
+                   'link: https://atelier801.com/topic?f=5&t=886189&p=1 e clique na aba "recrutamento".')
+
+
+@client.command(aliases=['staff'])
+async def Staff(ctx):
+    await ctx.send('Lista de toda a Staff >> https://atelier801.com/staff \n'
+                   'Tópico de anúncios da Staff BR >> https://atelier801.com/topic?f=5&t=788911&p=1'
+                   '\n\nSe quiser algo mais expecífico tente os comandos ?moderação, ?sentinela ou ?mapcrew.')
+
+
+@client.command(aliases=['moderação', 'moderacao', 'moderaçao', 'moderacão', 'Moderacao', 'Moderaçao', 'Moderacão'])
+async def Moderação(ctx):
+    await ctx.send('Informações sobre recrutamento >> https://atelier801.com/topic?f=6&t=855148&p=1#m1 \nFeedback '
+                   'para a moderação >> https://atelier801.com/topic?f=5&t=927074&p=1 \nCentral de banimentos >> '
+                   'https://atelier801.com/topic?f=5&t=814024&p=1')
+
+
+@client.command(aliases=['sentinela'])
+async def Sentinela(ctx):
+    await ctx.send('Informações sobre recrutamento >> https://atelier801.com/topic?f=6&t=891854&p=1#m1 \nFeedback '
+                   'para os sentinelas >> https://atelier801.com/topic?f=5&t=927060&p=1#m1 ')
+
+
+@client.command(aliases=['mapcrew'])
+async def Mapcrew(ctx):
+    await ctx.send('Informações sobre recrutamento >> https://atelier801.com/topic?f=6&t=846172&p=1#m1')
+
+
+@client.command(aliases=['evento'])
+async def Evento(ctx):
+    await ctx.send('Para saber mais informações sobre o evento atual no jogo, utilize esse link: '
+                   'https://atelier801.com/topic?f=6&t=879878&p=1 \nCaso você queira um tópico com mais discussão e '
+                   'mais movimento, pode tentar o tópico em inglês: https://atelier801.com/topic?f=6&t=888989&p=1 ('
+                   'não temos ligação com esse tópico).')
+
+
+@client.group(invoke_without_command=True)
+async def help(ctx):
+    em = discord.Embed(title="Help", description="lista com todos os comandos.")
+
+    em.add_field(name="Introdução", value="sobre")
+    em.add_field(name="Helpers", value="recrutamento, evento")
+    em.add_field(name="Staff", value="staff, moderação, sentinela, mapcrew")
+    em.add_field(name="Utilidaddes", value="clear")
+
+    await ctx.send(embed=em)
+
+@help.command()
+async def sobre(ctx):
+    em = discord.Embed(title='Sobre', description='Uma pequena descrição sobre o bot.')
+    await ctx.send(embed=em)
+
+
+
+@client.command(aliases=['Clear','clear', 'purge', 'Purge'])
+@commands.has_permissions(manage_messages=True)
+async def clear_messages(ctx, amount=2):
+    await ctx.channel.purge(limit=amount)
+    await ctx.send(f'Foram apagadas {amount} mensagens.')
+
+
+
+
+
+client.run(os.environ['token'])
