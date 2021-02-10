d2052d5438a859361e9c2126e084c1416252855e
Kanskje <kanskje7@protonmail.ch>
2021-02-10 19:48:48 -0300

    music update

--- bot.py
+++ bot.py
@@ -10,6 +10,50 @@ intents = intents.all()
 client = commands.Bot(command_prefix="?", intents=intents)
 client.remove_command("help")
 
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+
 filtered_words = ['fdp', 'arrombado', 'buceta', 'pinto', 'caralho', 'idiota', 'escroto', 'porra']
 
 
@@ -134,7 +178,91 @@ async def help(ctx):
     await ctx.send(embed=em)
 
 
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Latencia: {round(client.latency * 1000)}ms')
 
+
+@client.command()
+async def join(ctx):
+    if not ctx.message.author.voice:
+        await ctx.send("Você não está conectado a um canal de voz.")
+        return
+
+    else:
+        channel = ctx.message.author.voice.channel
+
+    await channel.connect()
+
+
+@client.command()
+async def queue_(ctx, url):
+    global queue
+
+    queue.append(url)
+    await ctx.send(f'`{url}` adicionado a fila!')
+
+
+@client.command()
+async def remove(ctx, number):
+    global queue
+
+    try:
+        del (queue[int(number)])
+        await ctx.send(f'Sua fila agora é `{queue}!`')
+
+    except:
+        await ctx.send('Sua fila ou está **vazia** ou o index está **sem espaço**')
+
+
+@client.command()
+async def play(ctx):
+    global queue
+
+    server = ctx.message.guild
+    voice_channel = server.voice_client
+
+    async with ctx.typing():
+        player = await YTDLSource.from_url(queue[0], loop=client.loop)
+        voice_channel.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+
+    await ctx.send('**Tocando agora:** {}'.format(player.title))
+    del (queue[0])
+
+
+@client.command()
+async def pause(ctx):
+    server = ctx.message.guild
+    voice_channel = server.voice_client
+
+    voice_channel.pause()
+
+
+@client.command()
+async def resume(ctx):
+    server = ctx.message.guild
+    voice_channel = server.voice_client
+
+    voice_channel.resume()
+
+
+@client.command()
+async def view(ctx):
+    await ctx.send(f'Sua fila agora é `{queue}!`')
+
+
+@client.command()
+async def leave(ctx):
+    voice_client = ctx.message.guild.voice_client
+    await voice_client.disconnect()
+
+
+@client.command()
+async def stop(ctx):
+    server = ctx.message.guild
+    voice_channel = server.voice_client
+
+    voice_channel.stop()
 
 
 @client.command(aliases=['Clear', 'clear', 'purge', 'Purge'])
