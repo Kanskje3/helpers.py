35068c294217aef5656462ec14defffe29eea756
Kanskje <kanskje7@protonmail.ch>
2021-02-10 17:58:18 -0300

    little update

--- bot.py
+++ bot.py
@@ -1,14 +1,15 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
+import youtube_dl
 import os
-import asyncio
-import random
+from random import choice
 
 intents = discord.Intents.default()
 intents.members = True
 intents = intents.all()
 client = commands.Bot(command_prefix="?", intents=intents)
 client.remove_command("help")
+status = ['?help para saber mais.']
 
 filtered_words = ['fdp', 'arrombado', 'buceta', 'pinto', 'caralho', 'idiota', 'escroto', 'porra']
 
@@ -27,7 +28,7 @@ async def on_message(msg):
     for word in filtered_words:
         if word in msg.content:
             await msg.delete()
-            break  ## stops it constantly spamming / continues the loop
+            break  # stops it constantly spamming / continues the loop
 
     if "noob" in msg.content.lower():
         await msg.add_reaction("<:mds:703304861575544962>")
@@ -133,101 +134,6 @@ async def help(ctx):
     await ctx.send(embed=em)
 
 
-def convert(time):
-    pos = ['s', 'm', 'h', 'd']
-    time_dict = {'s': 1, 'm': 60, 'h': 3600, 'd': 86400}
-    unit = time[-1]
-    if unit not in pos:
-        return -1
-    try:
-        val = int(time[:-1])
-    except:
-        return -2
-
-    return val * time_dict[unit]
-
-
-@client.command()
-@commands.has_role("Helpers")
-async def sorteio(ctx):
-    await ctx.send("Inicio do sorteio. Responda as seguintes perguntas em até 30 segundos.")
-    questions = ['Em qual canal o sorteio deve ser realizado?', 'Qual a duração do sorteio? (s|m|h|d)',
-                 'O que vai ser sorteado?']
-
-    answers = []
-    role = discord.utils.get(ctx.guild.roles, name="Sorteios")
-
-    def check(m):
-        return m.author == ctx.author and m.channel == ctx.channel
-
-    for i in questions:
-        await ctx.send(i)
-
-        try:
-            msg = await client.wait_for('message', timeout=30.0, check=check)
-        except asyncio.TimeoutError:
-            await ctx.send("Você demorou demais para responder. Tente novamente.")
-            return
-        else:
-            answers.append(msg.content)
-
-    try:
-        c_id = int(answers[0][2:-1])
-    except:
-        await ctx.send(f"Você não mencionou um canal. Tente {ctx.channel.mention} da próxima vez.")
-        return
-
-    channel = client.get_channel(c_id)
-
-    time = convert(answers[1])
-    if time == -1:
-        await ctx.send("Você não usou uma unidade de tempo correta (s|m|h|d). Tente novamente.")
-        return
-    elif time == -2:
-        await ctx.send("Você não usou um número inteiro para a hora. Tente novamente.")
-        return
-
-    prize = answers[2]
-
-    await ctx.send(
-        f"O sorteio será no canal {channel.mention} com duração de {answers[1]} a partir da data dessa mensagen.")
-
-    embed = discord.Embed(title="Sorteio!", description=f"{prize}")
-    embed.add_field(name="Feito por:", value=ctx.author.mention)
-    embed.set_footer(text=f'Termina em {answers[1]}')
-
-    my_msg = await channel.send(embed=embed)
-
-    await my_msg.add_reaction("<:alegre:703304861348790477>")
-    await channel.send(f'{role.mention} Novo sorteio! <:meucore:718465701626773594>. Esse sorteio terá a duração de '
-    f'{answers[1]} a partir da data desta mensagem. Reaja no "<:alegre:703304861348790477>" para participar!')
-    await asyncio.sleep(time)
-
-    new_message = await channel.fetch_message(my_msg.id)
-
-    users = await new_message.reactions[0].users().flatten()
-    users.pop(users.index(client.user))
-
-    winner = random.choice(users)
-
-    await channel.send(f"Parabéns {winner.mention}! Você ganhou {prize}!")
-
-
-@client.command()
-@commands.has_role("Helpers")
-async def refazer(ctx, channel: discord.TextChannel, id_: int):
-    try:
-        new_msg = await channel.fetch_message(id_)
-    except:
-        await ctx.send("O id foi enviado de maneira incorreta.")
-        return
-
-    users = await new_msg.reactions[0].users().flatten()
-    users.pop(users.index(client.user))
-
-    winner = random.choice(users)
-
-    await channel.send(f"Parabéns {winner.mention}, você é o novo vencedor!")
 
 
 
