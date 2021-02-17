223187de097513aed374d622ae5eb0cb98f9e6ba
Kanskje <kanskje7@protonmail.ch>
2021-02-17 20:07:16 -0300

    teste 2

--- bot.py
+++ bot.py
@@ -158,12 +158,12 @@ async def on_command_error(ctx, error):
 
 
 @client.command()
-async def nome(ctx, message):
-    channel = client.get_channel(id=782802548212891658)
+async def nome(message):
+    channel = client.get_channel(id=797141089998864465)
     characters = ['﹟', '₀', '₁', '₂', '₃', '₄', '₅', '₆', '₇', '₈', '₉']
     #for ch in message:
         #if ch.isdigit():
             #message.replace('#', '﹟')
-    await ctx.send(f'{message.content}')
+    await channel.send(f'{message.content}')
 
 client.run(os.environ['token'])
