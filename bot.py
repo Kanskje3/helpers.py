df4b666261d61d43c26f490ce23777f9148ccba5
Kanskje <kanskje7@protonmail.ch>
2021-03-04 12:31:16 -0300

    falha ping error, voltando ao normal

--- bot.py
+++ bot.py
@@ -155,7 +155,7 @@ async def on_command_error(ctx, error):
         embed.add_field(name=f'Invalid Permissions', value=f'You dont have {error.missing_perms} permissions.')
         await ctx.send(embed=embed)
     else:
-        embed.add_field(name=f':x: Terminal Error', value=f"```{error} {ctx.author.mention}```")
+        embed.add_field(name=f':x: Terminal Error', value=f"```{error}```")
         await ctx.send(embed=embed)
         raise error
 
