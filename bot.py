01d4ce334e2f9ed0381e61a5b481865d3d7c5a61
Kanskje <kanskje7@protonmail.ch>
2021-10-15 11:06:49 -0300

    trying to make both work, removed emojis

--- bot.py
+++ bot.py
@@ -27,46 +27,46 @@ async def on_ready():
 
 
 
-@client.event
-async def on_message(msg):
-    if "kaldt" in msg.content.lower():
-        if msg.author == client.user:
-            return
-        else:
-            await msg.channel.send("O que estão falando sobre mim? 👀")
-
-    if "austin" in msg.content.lower():
-        if msg.author == client.user:
-            return
-        else:
-            await msg.channel.send("Austin? O maior noob que ja vi")
-
-    if "noob" in msg.content.lower():
-        await msg.add_reaction("<:mds:703304861575544962>")
-        await msg.add_reaction("<:pikoh:606574166497558538>")
-
-    if "nub" in msg.content.lower():
-        await msg.add_reaction("<:mds:703304861575544962>")
-        await msg.add_reaction("<:pikoh:606574166497558538>")
-
-    if "formulário" in msg.content.lower():
-        await msg.add_reaction("<:desconfiadx:610229151840075786>")
-
-    if "formulario" in msg.content.lower():
-        await msg.add_reaction("<:desconfiadx:610229151840075786>")
-
-    if "austin" in msg.content.lower():
-        if msg.author == client.user:
-            return
-        else:
-            await msg.add_reaction("🇳")
-            await msg.add_reaction("🇴")
-            await msg.add_reaction("🇧")
-
-    if "pera" in msg.content.lower():
-        await msg.add_reaction("🍐")
-
-    await client.process_commands(msg)
+#@client.event
+#async def on_message(msg):
+#    if "kaldt" in msg.content.lower():
+#        if msg.author == client.user:
+#            return
+#        else:
+#            await msg.channel.send("O que estão falando sobre mim? 👀")
+#
+#    if "austin" in msg.content.lower():
+#        if msg.author == client.user:
+#            return
+#        else:
+#            await msg.channel.send("Austin? O maior noob que ja vi")
+#
+#    if "noob" in msg.content.lower():
+#        await msg.add_reaction("<:mds:703304861575544962>")
+#        await msg.add_reaction("<:pikoh:606574166497558538>")
+#
+#    if "nub" in msg.content.lower():
+#        await msg.add_reaction("<:mds:703304861575544962>")
+#        await msg.add_reaction("<:pikoh:606574166497558538>")
+#
+#    if "formulário" in msg.content.lower():
+#        await msg.add_reaction("<:desconfiadx:610229151840075786>")
+#
+#    if "formulario" in msg.content.lower():
+#        await msg.add_reaction("<:desconfiadx:610229151840075786>")
+#
+#    if "austin" in msg.content.lower():
+#        if msg.author == client.user:
+#            return
+#        else:
+#            await msg.add_reaction("🇳")
+#            await msg.add_reaction("🇴")
+#            await msg.add_reaction("🇧")
+#
+#    if "pera" in msg.content.lower():
+#        await msg.add_reaction("🍐")
+#
+#    await client.process_commands(msg)
 
 
 scam_links = ['google.com', 'yahoo.com', 'cat']
