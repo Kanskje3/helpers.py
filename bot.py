581921eed08b76da882e026d468b2b30b5d83fa6
Kanskje <kanskje7@protonmail.ch>
2021-10-14 23:49:26 -0300

    almost done filter

--- bot.py
+++ bot.py
@@ -29,12 +29,13 @@ filtered_words = ['google.com', 'yahoo.com']
 
 @client.event
 async def on_message(msg):
-    if not msg.author == client.user:
-        for word in filtered_words:
-            if word in msg.content:
-                await msg.delete()
+    if msg.author == client.user:
+        return
+    for word in filtered_words:
+        if word in msg.content:
+            await msg.delete()
 
-                break  # stops it constantly spamming / continues the loop
+            break  # stops it constantly spamming / continues the loop
 
 
 @client.event
